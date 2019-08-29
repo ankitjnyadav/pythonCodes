d14a6c69b5bfdb18169c72a87f8d33ac116b2659
ABHISHEK DOBHAL <dovalabhi01@gmail.com>
2019-08-29 21:30:11 +0530

    week-4 assignments

--- frequency.py
+++ frequency.py
@@ -1,3 +1,21 @@
+'''
+Write a Python function frequency(l) that takes as input a list of integers and returns a pair of the form(minfreqlist, maxfreqlist) where
+
+minfreqlist is a list of numbers with minimum frequency in l, sorted in ascending order
+
+maxfreqlist is a list of numbers with maximum frequency in l, sorted in ascending
+
+frequency([13,12,11,13,14,13,7,11,13,14,12])
+([7], [13])
+
+frequency([13,12,11,13,14,13,7,11,13,14,12,14,14])
+([7], [13, 14])
+
+frequency([13,12,11,13,14,13,7,11,13,14,12,14,14,7])
+([7, 11, 12], [13, 14])
+'''
+
+
 def frequency(l):
     freq_table = {}
     min_v, max_v = 0, 0
